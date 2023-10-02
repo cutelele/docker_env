d7298bd459280c0ba3561817dd23b6d2e3d76de2
cutelele <kefon@163.com>
2023-10-02 23:23:12 +0800

    ok

--- emby/nginx/conf.d/emby.js
+++ emby/nginx/conf.d/emby.js
@@ -18,7 +18,7 @@ async function redirect2Pan(r) {
 
     const itemInfoUri = `${embyHost}/Items/${itemId}/PlaybackInfo?MediaSourceId=${mediaSourceId}&api_key=${api_key}`;
     r.warn(`itemInfoUri: ${itemInfoUri}`);
-    const embyRes = await fetchEmbyFilePath(itemInfoUri);
+    let embyRes = await fetchEmbyFilePath(itemInfoUri);
     if (embyRes.startsWith('error')) {
         r.error(embyRes);
         r.return(500, embyRes);
