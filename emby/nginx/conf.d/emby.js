ee92ff5d473bd99bcec497fd75e9d6960b02f724
cutelele <kefon@163.com>
2023-10-04 19:19:32 +0800

    加入emby本地库支持

--- emby/nginx/conf.d/emby.js
+++ emby/nginx/conf.d/emby.js
@@ -29,11 +29,20 @@ async function redirect2Pan(r) {
 
     if (!embyRes.startsWith('error')) {
         if(my_xiaoya_addr!=""&&rep_text!=""){//此处判断是否需要地址转换
-            let addrs = my_xiaoya_addr.split("|");
-            let randomIndex = Math.floor(Math.random() * addrs.length);//随机方式负载均衡
-            embyRes = embyRes.replace(rep_text, addrs[randomIndex]);
+            if (embyRes.indexOf(rep_text)!=-1){
+                let addrs = my_xiaoya_addr.split("|");
+                let randomIndex = Math.floor(Math.random() * addrs.length);//随机方式负载均衡
+                embyRes = embyRes.replace(rep_text, addrs[randomIndex]);
+                r.warn(`redirect to 302: ${embyRes}`);
+                r.return(302, embyRes);
+            }else{//兼容emby本地库,不做302跳转
+                r.warn(`redirect to 200: ${embyRes}`);
+                r.return(200, embyRes); 
+            }
         }
-        r.warn(`redirect to end: ${embyRes}`);
+        //运行到此处说明用户自己搞定了.strm内容替换,这里直接302,如果这种情况下还需要对emby本地库进行支持,建议走转换配置
+        //把my_xiaoya_addr和rep_text配置成和.strm一致的地址,利用不改变.strm实际内容但又跑了转换逻辑,达到筛选出emby本地库并进行支持的目的.
+        r.warn(`redirect not trans to 302: ${embyRes}`);
         r.return(302, embyRes);
         return;
     }
