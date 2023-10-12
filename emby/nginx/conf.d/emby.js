e28b37d86786e03facb3f624b0de11cdd5e71f4c
cutelele <kefon@163.com>
2023-10-12 14:36:24 +0800

    增加支持fileball

--- emby/nginx/conf.d/emby.js
+++ emby/nginx/conf.d/emby.js
@@ -1,14 +1,16 @@
 //查看日志: "docker logs -f -n 10 nginx 2>&1  | grep js:"
+//多个地址负载均衡,地址之间请用“|”隔开 例:const my_xiaoya_addr = "http://aaaaaa:5678|http://bbbbb:5678";注意最后的地址不要带“|”
+const my_xiaoya_addr = "http://xiaoya.lu2728.com";//默认兼容单地址方式,如需关闭地址转换此处请留空 例: const my_xiaoya_addr = "";
+const rep_text = "DOCKER_ADDRESS";//如需关闭地址转换此处请留空 例: const rep_text = "";
+//以上两处变量同时配置,方可启用地址转换功能.
+const embyHost = 'http://172.20.0.2:8096';
 async function redirect2Pan(r) {
 
-    //多个地址负载均衡,地址之间请用“|”隔开 例:const my_xiaoya_addr = "http://aaaaaa:5678|http://bbbbb:5678";注意最后的地址不要带“|”
-    const my_xiaoya_addr = "http://xxxxxxx:5678";//默认兼容单地址方式,如需关闭地址转换此处请留空 例: const my_xiaoya_addr = "";
-    const rep_text = "DOCKER_ADDRESS";//如需关闭地址转换此处请留空 例: const rep_text = "";
-    //以上两处变量同时配置,方可启用地址转换功能.
-    const embyHost = 'http://192.168.3.11:8096';
+
     const regex = /[A-Za-z0-9]+/g;
     const itemId = r.uri.replace('emby', '').replace(/-/g, '').match(regex)[1]; 
     const mediaSourceId = r.args.MediaSourceId;
+
     let api_key = r.args.api_key;
 
     //infuse用户需要填写下面的api_key, 感谢@amwamw968
@@ -28,15 +30,10 @@ async function redirect2Pan(r) {
     r.warn(`mount emby file path: ${embyRes}`);
 
     if (!embyRes.startsWith('error')) {
-        if(my_xiaoya_addr!=""&&rep_text!=""){//此处判断是否需要地址转换
-            let addrs = my_xiaoya_addr.split("|");
-            let randomIndex = Math.floor(Math.random() * addrs.length);//随机方式负载均衡
-            embyRes = embyRes.replace(rep_text, addrs[randomIndex]);
-            r.warn(`embyRes replace end: ${embyRes}`);
-        }
+        embyRes =transferStr(embyRes);
         if (embyRes.indexOf("http")!=-1){
-            r.warn(`redirect to 302: ${embyRes}`);
-            r.return(302, embyRes);
+            r.warn(`redirect to 302: ${encodeURI(embyRes)}`);
+            r.return(302, `${encodeURI(embyRes)}`);
             return;
         }
         r.warn(`redirect to source path`);
@@ -57,6 +54,7 @@ async function redirect2Pan(r) {
     r.internalRedirect("@backend");
     return;
 }
+
 async function fetchEmbyFilePath(itemInfoUri) {
     try {
         const res = await ngx.fetch(itemInfoUri, {
@@ -82,4 +80,59 @@ async function fetchEmbyFilePath(itemInfoUri) {
         return (`error: emby_api fetch mediaItemInfo failed,  ${error}`);
     }
 }
-export default { redirect2Pan };
+function transferStr(str){
+    if(my_xiaoya_addr!=""&&rep_text!=""){//此处判断是否需要地址转换
+        let addrs = my_xiaoya_addr.split("|");
+        let randomIndex = Math.floor(Math.random() * addrs.length);//随机方式负载均衡
+        str = str.replace(rep_text, addrs[randomIndex]);
+    }
+    return str;
+}
+
+function generateUrl(r, host, uri) {
+  let url = host + uri;
+  let isFirst = true;
+  for (const key in r.args) {
+    url += isFirst ? "?" : "&";
+    url += `${key}=${r.args[key]}`;
+    isFirst = false;
+  }
+  return url;
+}
+async function transferPlaybackInfo(r) {
+
+	const cloneHeaders = {};
+	for (const key in r.headersIn) {
+		cloneHeaders[key] = r.headersIn[key].replace(/-/g, '');//replace(/"/g, '\\"');
+		r.warn(`playbackinfo reuqest clone header ${key}: ${cloneHeaders[key]}`);
+	}
+	const query = generateUrl(r, "", "").substring(1);
+	const queryPath = `${embyHost}${r.uri}?${encodeURI(query)}&api_key=df306dbc4c484e538207cae938cc98ec`;
+	r.warn(`queryPath===: ${queryPath}`);
+	const response = await ngx.fetch(queryPath,{
+    method: r.method,
+    args: query,
+    max_response_body_size: 65535,
+  });
+    for(const key in response){
+		r.warn(`response ${key}: ${response[key]}`);
+    }
+    if (response.ok) {
+        const result = await response.json();
+        if (result === null || result === undefined) {
+            r.internalRedirect("@backend");
+            return;
+        }
+        result.MediaSources[0].Path=transferStr(result.MediaSources[0].Path);
+        r.warn(`result: ${JSON.stringify(result)}`);
+        r.return(200,`${JSON.stringify(result)}`); 
+    }else {
+        r.internalRedirect("@backend");
+    	return;
+    }
+    r.internalRedirect("@backend");
+	return;
+}
+
+
+export default {redirect2Pan,transferPlaybackInfo};
